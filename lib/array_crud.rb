21f7f7fa3ae51475753d508ec4df0728179b3124
Cheryl K <cherylkelson@icloud.com>
2018-09-12 20:01:54 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,7 +4,7 @@ def create_an_empty_array
 end
 
 def create_an_array  
-  [poodle, giraffe, "pekinese", mutt, 2]
+  [poodle, giraffe, "pekinese", mutt]
 end
 
 def add_element_to_end_of_array(array, element)
